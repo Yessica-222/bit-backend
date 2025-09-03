2758e0cba4072f30eb608f6eaa9e950ae74f953e
Yessica Alexandra Conejo Muñoz <munozyessica2001@gmail.com>
2025-09-03 14:42:45 -0500

    Se agrega archivo server.js con todas sus rutas

--- src/server.js
+++ src/server.js
@@ -1,20 +1,41 @@
-import 'dotenv/config';
-import connectDB from './config/db.js';
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 import morgan from 'morgan';
-import productsRouter from './routes/product.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Importamos las rutas
+import usersRouter from './routes/users.js';
+import productsRouter from "./routes/products.js";
+import cartRouter from './routes/cart.js';
+import appointmentRouter from './routes/appointment.js';
+import serviceRouter from './routes/service.js';
+import invoiceRouter from './routes/invoice.js';
+import paymentRouter from './routes/payment.js';
+
+dotenv.config(); // Cargar variables de entorno
 const server = express();
-const host = process.env.HOST;
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-connectDB();
+// Middlewares globales
+server.use(express.json()); // Permite leer JSON en las peticiones
+server.use(morgan('dev')); // Logger de peticiones HTTP
+server.use(cors()); // Habilita CORS para peticiones desde otros orígenes
 
-server.use(express.json());
-server.use(morgan('dev'));
-
-// Rutas
+// Definición de rutas principales
+server.use('/api/users', usersRouter);
 server.use('/api/products', productsRouter);
+server.use('/api/cart', cartRouter);
+server.use('/api/appointment', appointmentRouter);
+server.use('/api/services', serviceRouter);
+server.use('/api/invoices', invoiceRouter);
+server.use('/api/payment', paymentRouter);
 
-server.listen(port, () => {
-  console.log(`Server is running at ${host} on port ${port}`);
-});
+// Conexión a MongoDB y arranque del servidor
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log(' Conectado a MongoDB');
+    server.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+  })
+  .catch((err) => console.error(' Error al conectar MongoDB:', err));
